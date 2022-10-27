8ba53bbc9f9adbdac491a54b247d57f1c2818d4c
Mark <kalmark@mail.ru>
2022-10-27 21:32:32 +0300

    Add Ex_46_FillArray

---
+++ Ex_46_FillArray/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array= new int[m,n];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
