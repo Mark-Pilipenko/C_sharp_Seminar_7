57acd9a386eeb18eed7c0ab9566add6525a9063e
Mark <kalmark@mail.ru>
2022-10-27 21:52:57 +0300

    Add Ex49_EvenIndex

---
+++ Ex49_EvenIndex/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные,
+//  и замените эти элементы на их квадраты.
+
+
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array= new int[m,n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 ==0 && j % 2 == 0) matrix[i,j] = matrix[i,j] * matrix[i,j];
+        }
+    }
+}
